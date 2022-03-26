e54369e65a51f0831335d1bc5fd7866eac507189
Henrique <you@example.com>
2022-03-26 07:24:25 -0300

    tentativa

---
+++ main.py
@@ -0,0 +1,10 @@
+print('Oi')
+a = int(input('Diga o primeiro numero'))
+print('soma','subtração')
+b = input('Diga a operação\n')
+c = int(input('Diga seu segundo numero'))
+
+
+
+if b == 'soma':
+    print('A respoosta é', a + c)
